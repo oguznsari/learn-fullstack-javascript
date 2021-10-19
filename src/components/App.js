c2a1747dbbc3ab5985e44284bbd7f6eb2ee0a790
Oğuzhan <oguzn.sari@gmail.com>
2021-10-20 00:57:18 +0300

    state v2

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,13 @@ import React from 'react';
 import Header from './Header';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { test: 42 };
-  }
+  state = { pageHeader: 'Naming Contest' };
   render() {
     return (
       <div className="App">
-        <Header message="Naming Contests" />
+        <Header message={this.state.pageHeader} />
         <div>
-          {this.state.test}
+          - - - - -
         </div>
       </div>
     );
