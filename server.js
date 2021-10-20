326b3bd91c858aa5cf582beede79463ca8245cd5
Oğuzhan <oguzn.sari@gmail.com>
2021-10-20 23:46:01 +0300

    styles added with Sass-node

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import config from './config';
 import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path'
 
 import express from 'express';
 const server = express();
 
 server.set('view engine', 'ejs');
+
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),                    // dir - read the sass from
+  dest: path.join(__dirname, 'public')                  // dir - write css to
+}));
 
 server.get('/', (req, res) => {
   res.render('index', {
