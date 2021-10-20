4fbbf4a0225152a15db3d492e8f052aebc58356d
Oğuzhan <oguzn.sari@gmail.com>
2021-10-20 22:44:16 +0300

    ContestPreview with the 1st contest

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
-  state = { pageHeader: 'Naming Contest' };
+  state = { pageHeader: 'Naming Contests' };
   render() {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          - - - - -
+          <ContestPreview {...this.props.contests[0]} />
         </div>
       </div>
     );
