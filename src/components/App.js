9a575eda06b397e5a9066cb17da51d681f6bc703
Oğuzhan <oguzn.sari@gmail.com>
2021-10-28 14:10:09 +0300

    navigate back to Contests List with link

--- src/components/App.js
+++ src/components/App.js
@@ -32,6 +32,19 @@ class App extends React.Component {
     });  
   };
 
+  fetchContestList = () => {
+    pushState(
+      { currentContestId: null },
+      '/'
+    );
+    api.fetchContestList().then(contests => {
+      this.setState({
+        currentContestId: null,
+        contests
+      });
+    });  
+  };
+
   pageHeader() {
     if (this.state.currentContestId) {
       return this.currentContest().contestName;
@@ -45,7 +58,9 @@ class App extends React.Component {
 
   currentContent() {
     if(this.state.currentContestId) {
-      return <Contest {... this.currentContest()} />;
+      return <Contest
+                contestListClick={this.fetchContestList}
+                {... this.currentContest()} />;
     } else {
       return <ContestList 
                 onContestClick={this.fetchContest}
