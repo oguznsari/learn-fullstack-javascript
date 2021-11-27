1b2c6853166793b3985985ae28a79d7d7292786d
Oğuzhan <oguzn.sari@gmail.com>
2021-11-27 13:58:20 +0300

    convert the app to use _id instead of id

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ server.get(['/', '/contest/:contestId'], (req, res) => {
         initialData
       });
     })
-    .catch(console.error);
+    .catch(error => {
+      console.error(error);
+      res.status(404).send('Bad Request.');
+    });
 
 });
 
